89c0c25e962150e57d605ce700c841d7c94c5f7e
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 14:45:32 +0530

    [Rupali]Feat:find requency of sentence

--- Program.cs
+++ Program.cs
@@ -7,6 +7,23 @@ namespace HashTableProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Program");
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+
+            //Add data
+            myMapNode.Add("0", "To");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "To");
+            myMapNode.Add("3", "or");
+            myMapNode.Add("4", "not");
+            myMapNode.Add("5", "be");
+
+            //Get Value and Display
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th index  value: " + hash5);
+
+            //Get Value and Display
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd index  value: " + hash2);
         }
     }
 }
